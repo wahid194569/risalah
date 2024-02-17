71ec7be40422dca722bfe6a2cdd485a08bd8fa20
Abdul Wahid <71002245+wahid194569@users.noreply.github.com>
2024-02-17 17:45:54 +0700

    configure css, and js link or script
    Mau konekin tampilannya biar bagus dashboardnya

---
+++ resources/views/Originalhome.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>SIRI</title>
+</head>
+<body>
+
+    <h1>DASHBOARD</h1>
+    <h2>WELCOME TO SIRI - Sistem Informasi Pengajuan Judul Risalah</h2>
+
+</body>
+</html>
