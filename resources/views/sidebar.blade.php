785ae633b2e8686e81ed80ce4197f854a8d6ce92
A. Muzacky Naufal Ammar <107091692+am-naufal@users.noreply.github.com>
2024-02-21 13:52:05 +0700

    Update sidebar.blade.php

--- resources/views/sidebar.blade.php
+++ resources/views/sidebar.blade.php
@@ -6,8 +6,11 @@
         </a>
         <div class="d-flex align-items-center ms-4 mb-4">
             <div class="position-relative">
-                <img class="rounded-circle" src="{{ asset('img/user.jpg')}}" alt="" style="width: 40px; height: 40px;">
-                <div class="bg-success rounded-circle border border-2 border-white position-absolute end-0 bottom-0 p-1"></div>
+                <img class="rounded-circle" src="{{ asset('img/user.jpg') }}" alt=""
+                    style="width: 40px; height: 40px;">
+                <div
+                    class="bg-success rounded-circle border border-2 border-white position-absolute end-0 bottom-0 p-1">
+                </div>
             </div>
             <div class="ms-3">
                 <h6 class="mb-0">Jhon Doe</h6>
@@ -16,7 +19,8 @@
         </div>
         <div class="navbar-nav w-100">
             <a href="/" class="nav-item nav-link active"><i class="fa fa-tachometer-alt me-2"></i>Dashboard</a>
-            <a href="{{ route('pengajuan.form') }}" class="nav-item nav-link "><i class="fa fa fa-keyboard me-2 me-2"></i>Pengajuan Judul</a>
+            <a href="{{ route('pengajuan') }}" class="nav-item nav-link "><i
+                    class="fa fa fa-keyboard me-2 me-2"></i>Pengajuan Judul</a>
             <a href="{{ route('pengajuan') }}" class="nav-item nav-link"><i class="fa fa-th me-2"></i>List Judul</a>
         </div>
     </nav>
