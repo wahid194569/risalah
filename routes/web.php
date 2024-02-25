45e65b0344eb30846083146c904f221d90462e31
A. Muzacky Naufal Ammar <107091692+am-naufal@users.noreply.github.com>
2024-02-25 14:38:28 +0700

    rout fix

--- routes/web.php
+++ routes/web.php
@@ -94,10 +94,10 @@ Route::post('/login', [AuthController::class, 'login'])->name('login');
 // Logout
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 // Route::post('/', [AuthController::class, 'logout'])->name('logout');
-<<<<<<< Updated upstream
-=======
+
+
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
->>>>>>> Stashed changes
+
 
 // Submit
 Route::post('/submit-form', [FormController::class, 'submit'])->name('submit.form');
